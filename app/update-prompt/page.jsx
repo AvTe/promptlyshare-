1976dd4357d4995f1791929078bd84a52ed8bc2c
Amit Vishwakarma <52880868+AvTe@users.noreply.github.com>
2024-09-18 21:58:32 +0530

    error solve

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,71 +1,16 @@
-import { Suspense } from 'react';
-import dynamic from 'next/dynamic';
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 
-const Form = dynamic(() => import('@components/Form'), {
+const EditPrompt = dynamic(() => import("@components/EditPrompt"), {
   suspense: true,
 });
 
-const UpdatePrompt = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
-
-  const [post, setPost] = useState({ prompt: "", tag: "" });
-  const [submitting, setIsSubmitting] = useState(false);
-
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`);
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-
-    if (promptId) getPromptDetails();
-  }, [promptId]);
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-
-    if (!promptId) return alert("Missing PromptId!");
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      } else {
-        console.error('Failed to update prompt:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error updating prompt:', error);
-    } finally {
-      setIsSubmitting(false);
-    }
-  };
-
+const Page = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Form
-        type='Edit'
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
+    <Suspense>
+      <EditPrompt />
     </Suspense>
   );
 };
 
-export default UpdatePrompt;
+export default Page;
--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
       // appDir: true,
       // serverComponentsExternalPackages: ["mongoose"],
     // },
+    experimental: {
+      // This option allows you to opt-out of Server Components for pages that use `getServerSideProps` or `getInitialProps`
+      appDir: true,
+      // This option allows you to opt-out of Server Components for pages that use `getServerSideProps` or `getInitialProps`
+      // and instead render them as Static Site Generation (SSG) pages.
+      // serverComponentsExternalPackages: ["mongoose"],
+    },
     images: {
       domains: ['lh3.googleusercontent.com'],
     },
